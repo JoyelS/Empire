edc0384971d301e096901cd8c4c7e1baaa031460
parker57 <parker57@uni.coventry.ac.uk>
2017-03-06 03:15:24 +0000

    Update feshgame.cpp

--- feshgame.cpp
+++ feshgame.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class USER{
 	public:
-	
+
 		//Default constructor
 		USER(string name, int h, int a, double m){
 			change_name(name);
@@ -16,7 +16,6 @@ class USER{
 			/*change_health(health);
 			change_attack(attack);
 			change_money(money);*/
-			
 			}
 
 		//Methods to change and retreive/view name.
@@ -30,11 +29,9 @@ class USER{
 		void change_attack(int x){//made an int to test method to permanantly modify value.
 			this->attack+=x;
 			//return this->attack;
-		
 			}
-
-		void update_attack(){
-			attack=1;};
+		void update_attack(int x){
+			attack=x;};
 		int get_attack(){
 			return attack;
 			}
@@ -63,11 +60,41 @@ class USER{
 		double money;
 
 };
-void fight(USER user){
+int fight(USER user){
+
+    //static fight sequence, will only ever be against goblin, could be cloned for hunt, no loot or stat augmentation other than loss of health. To apply generally see python prototype.
+    //Health of the user is not returned.
 
 	cout<<"RUN FIGHT SEQUENCE"<<endl;
-	
-	user.change_attack(1);
+    USER goblin("Goblin",5,1,1.50);
+
+    cout<<user.get_name()<<" fights "<<goblin.get_name()<<endl;
+    cout<<"------------------------------------------------"<<endl;
+
+    cout<<"------------------------------------------------"<<endl;
+    while (user.get_health()>0 &&goblin.get_health()>0){
+        cout<<user.get_name()<<" attacks "<<goblin.get_name()<<" for "<<user.get_attack()<<endl;
+        goblin.change_health(-1*user.get_attack());
+        goblin.get_status();
+        cout<<"------------------------------------------------"<<endl;
+
+        if (goblin.get_health()>0){
+            cout<<goblin.get_name()<<" attacks "<<user.get_name()<<" for "<<goblin.get_attack()<<endl;
+            user.change_health(-1*goblin.get_attack());
+            user.get_status();
+        }
+    }
+
+
+    //could use switch statements here.
+
+    if (user.get_health()<0){
+        cout<<user.get_name()<<" was killed."<<endl;
+    }
+    else{
+        cout<<goblin.get_name()<<" was killed."<<endl;
+    }
+    return user.get_health();//returning altered variable does not work,
 }
 
 void exit(){
@@ -76,12 +103,12 @@ void exit(){
 }
 
 void hunt(USER user){
-	
+
 	cout<<"RUN HUNT SEQUENCE"<<endl;
 	user.get_status();
 	user.change_health(1);
 //testing, as user attributes can not be changed from these functions for some reason.
-//DOES CHANGE JUST DOESN'T PERMANANTLY MODIFY VALUE, PERHAPS THE CHANGE NEEDS TO BE RETURNED? 
+//DOES CHANGE JUST DOESN'T PERMANANTLY MODIFY VALUE, PERHAPS THE CHANGE NEEDS TO BE RETURNED?
 	//return user; ---What type of funciton could return an object? or do they need to be methods of user?
 	user.get_status();
 }
@@ -91,55 +118,44 @@ void shop(USER user){//Changed int for void
 	cout<<"RUN SHOP SEQUENCE"<<endl;
 }
 
+string UserIn;
 void  menu(USER user)
 {
-string UserIn;
-
 user.get_status();
 
 cout<<"[F]ight"<<endl<<"[E]xit"<<endl
 <<"[S]hop"<<endl<<"[H]unt"<<endl;
 
 cin>>UserIn;
-
 //transform(UserIn.begin(),UserIn.end(),UserIn.begin(),::tolower);
-
-
 UserIn=UserIn[0];
 UserIn=tolower(UserIn[0]);
 
 	if (UserIn=="f"){
 		cout<<"FIGHT"<<endl;
 		fight(user);//Fight and other modules should return values which are used to alter user stats.
-	
+
 		//user.update_attack(); - update_attack can be used to set attack to a specific value, change_attack is used to alter it by the agument.
 		//user.get_status(); - for testing.
-
-		user.change_attack(1);//for example here attack is increased by 1.
-		menu(user);
 			}
 
 	else if (UserIn=="s"){
 		cout<<"SHOP"<<endl;
 		shop(user);
 		user.change_money(0.01);
-		menu(user);
 			}
 
 	else if (UserIn=="e"){
 		cout<<"EXIT"<<endl;
-		exit();
 			}
 
 	else if (UserIn=="h"){
 		cout<<"HUNT"<<endl;
 		hunt(user);
 		user.change_health(1);
-		menu(user);
 			}
 	else{
 		cout<<"INVALID INPUT"<<endl;
-		menu(user);
 			}
 }
 
@@ -150,8 +166,14 @@ cout<<"What is your Username? ";
 cin>>Username;
 cout<<"\nUsername is: "<<Username<<endl;
 
-USER user(Username, 10,5,0.01);
+USER user(Username, 10,2,0.01);
 
-menu(user);
+do{
+    menu(user);
+}while (UserIn!="e");
+exit();
+
+//save user stats to database?
+
 return 0;
 }
