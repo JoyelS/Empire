9f2448fd4b02d97f5561b6751183c1eea78b6f25
parker57 <parker57@uni.coventry.ac.uk>
2017-03-07 04:17:59 +0000

    Update MAIN.cpp

--- FESH/MAIN.cpp
+++ FESH/MAIN.cpp
@@ -1,94 +1,137 @@
-//Potential database tables for user stats, shop inventory and enemies (animals and monsters) - Ideally one table for each on the same database.
-//Contains 'main menu' function, fight, exit, shop and hunt modules can be localised or even better made in other files. To localise them, simply elaborate on the respective void functions.
-
-#include "user.h"
-
 #include <iostream>
 #include <string>
-#include <algorithm>
+
+#include <unistd.h>//for sleep ability
+
 
 using namespace std;
 
-USER::USER (string,int,int,double){
-	this->name = "test";
-	this->attack = 7;
-	this->health = 10;
-	this->money = 0;
+unsigned int pause = 600000; //pause time in microseconds for engagement functions.
 
-}
+class CHARACTER{//CHARACTER CLASS, CAN BE USED GENERICALLY FOR ANIMALS, USERS, MONSTERS, ETC.
+public:
+    //default constructor.
+    CHARACTER(string N="Default name", int H=10, int A=1, double M=0.01){
+        name=N;
+        health=H;
+        attack=A;
+        money=M;
+    }
 
-void USER::display() {
+    void get_status()
+    {cout<<name<<" has "<<health<<" health, "<<attack<<" attack & £"<<money<<endl;}
 
-cout<<this->health<<endl;
+    void change_name(string new_name){
+        name=new_name;
+    }
 
-}
+    void change_health(int x){ //specified as non void in case it will be useful to produce a return.
+        health+=x;
+    }
+    void change_attack(int x){ //specified as non void in case it will be useful to produce a return.
+        attack+=x;
+    }
+    void change_money(double x){ //specified as non void in case it will be useful to produce a return.
+        money+=x;
+    }
 
-void fight(){
+    //variables to view private attributes.
+    string get_name(){
+        return name;
+    }
+    int get_health(){
+        return health;
+    }
+    int get_attack(){
+        return attack;
+    }
+    double get_money(){
+        return money;
+    }
 
-	cout<<"RUN FIGHT SEQUENCE"<<endl;
-}
+private:
+    string name;
+    int health;
+    int attack;
+    double money;
+};
 
-void exit(){
+//---------------------------------- END OF CHARACTER CLASS ----------------------------------
+//Test creations of character objects.
+CHARACTER user("a", 100,3,0.01);
+CHARACTER goblin("GOBLIN",10,2,3.50);
 
-	cout<<"RUN EXIT SEQUENCE"<<endl;
-}
+//MENU FUNCTION:
+string UserIn;//Declaration of user input for menu function.
 
-void hunt(){
-
-	cout<<"RUN HUNT SEQUENCE"<<endl;
-}
-
-void shop(){//Changed int for void
-
-	cout<<"RUN SHOP SEQUENCE"<<endl;
-}
-
-void menu(){
-string UserIn;
+void  menu()
+{
+user.get_status();
 
 cout<<"[F]ight"<<endl<<"[E]xit"<<endl
 <<"[S]hop"<<endl<<"[H]unt"<<endl;
 
 cin>>UserIn;
 
-//transform(UserIn.begin(),UserIn.end(),UserIn.begin(),::tolower);
-
-
 UserIn=UserIn[0];
 UserIn=tolower(UserIn[0]);
 
 	if (UserIn=="f"){
 		cout<<"FIGHT"<<endl;
-		fight();
-		menu();
-			}
+		//SIMPLE FIGHT NEEDS ELABORATION. No loot, No max health etc.
+		//Random loot would be good.
+		cout<<user.get_name()<<" fights "<<goblin.get_name()<<endl;
+        cout<<"------------------------------------------------"<<endl;
+        goblin.get_status();
+
+        cout<<"------------------------------------------------"<<endl;
+        while (user.get_health()>0 &&goblin.get_health()>0){
+            cout<<user.get_name()<<" attacks "<<goblin.get_name()<<" for "<<user.get_attack()<<endl;
+            goblin.change_health(-1*user.get_attack());
+            goblin.get_status();
+            cout<<"------------------------------------------------"<<endl;
+
+            if (goblin.get_health()>0){
+                cout<<goblin.get_name()<<" attacks "<<user.get_name()<<" for "<<goblin.get_attack()<<endl;
+                user.change_health(-1*goblin.get_attack());
+                user.get_status();
+                }
+            usleep(pause);
+            }
+	}
 
 	else if (UserIn=="s"){
 		cout<<"SHOP"<<endl;
-		shop();
-		menu();
+
+		user.change_money(1.00);
 			}
 
 	else if (UserIn=="e"){
 		cout<<"EXIT"<<endl;
-		exit();
 			}
 
 	else if (UserIn=="h"){
 		cout<<"HUNT"<<endl;
-		hunt();
-		menu();
+		user.change_health(1);
 			}
 	else{
 		cout<<"INVALID INPUT"<<endl;
-		menu();
 			}
 }
+//---------------------------------- END OF MENU FUNCTION ----------------------------------
 
-int main()
-{
-USER user;
-user.display();
-menu();
+
+int main(){
+
+string Username;//User picks his name which is passed to the USER class, user should be checked in database to see if it exists, if so it should assume those stats.
+cout<<"What is your Username? ";
+cin>>Username;
+cout<<"\nUsername is: "<<Username<<endl;
+user.change_name(Username);
+
+do{
+    menu();
+}while (UserIn!="e");
+//exit();
 return 0;
 }
